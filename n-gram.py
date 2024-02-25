b24fac2023ea59eb1e767eba6ca9702fcf32ecf3
qiuxuhan <xzh121022@126.com>
2024-02-25 21:46:51 +0800

    cc

---
+++ n-gram.py
@@ -0,0 +1,13 @@
+
+# # 数值列表
+# num_list = [1, 3, 2, 1, 5, 3]
+#
+# # 获取2-gram特征的函数
+# def get_bigrams(lst):
+#     bigrams = [(lst[i], lst[i + 1]) for i in range(len(lst) - 1)]
+#     return bigrams
+#
+# bigrams = get_bigrams(num_list)
+#
+# print(bigrams)
+
